27ca7e05211c2247b498982900cb983dc7354833
Artem-Parashchenko <artem99paraschenko@gmail.com>
2022-07-13 18:28:04 +0300

    Add stages section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
-// const getS = (selector, i = 0) => document.querySelectorAll(selector)[i];
-
+// SERVICE SECTION //
 document.querySelectorAll('.service__item .switch').forEach(element => {
   element.addEventListener('click', () => {
     element.closest('.service__item').classList.toggle('open');
   })
 });
+
+document.querySelectorAll('.service__item .text').forEach(element => {
+  const parent = element.closest('.service__item');
+  const height = parseInt(window.getComputedStyle(element, null).height);
+  parent.style.setProperty('--height', `${height + 121}px`);
+})
+
+
+//STAGES SECTION //
+let currentCard = 0;
+const stages = document.querySelectorAll('.stage');
+const stagesList = document.querySelector('.stages__list')
+const stagesBtn = document.querySelector('.stages__buttons');
+const btnRight = document.querySelector('.stages .btn-right');
+const btnLeft = document.querySelector('.stages .btn-left');
+const maxCard = stages.length - 1;
+
+stagesBtn.addEventListener('click', (e) => {
+  const stageWidth = parseInt(window.getComputedStyle(stages[0], null).width);
+  const stagesGap = parseInt(window.getComputedStyle(stagesList, null).gap);
+
+  switch (e.target) {
+    case btnLeft:
+      console.log(currentCard)
+      currentCard < maxCard ? currentCard++ : currentCard = maxCard;
+      console.log('left')
+      console.log(currentCard)
+      break;
+    case btnRight:
+      currentCard > 0 ? currentCard-- : currentCard = 0;
+      console.log('rght')
+      break;
+    default:
+      return;
+  }
+
+  stagesList.style.transform = `translateX(-${(stageWidth + stagesGap) * currentCard}px)`;
+})
